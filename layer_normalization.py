d47a98903449da2442e9d2ae3b4544206d978358
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-07-10 15:03:17 +0300

    layer normalization module added.

---
+++ layer_normalization.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10 ** -6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))  # multiplied
+        self.bias = nn.Parameter(torch.zeros(1))  # added
+
+    def forward(self, x):
+        # calculate mean for last dim of batch
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
